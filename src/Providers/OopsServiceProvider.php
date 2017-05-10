d5423a5b1e604c62b6c766c9a9be5f4892360703
Andy Muth <andy@kps3.com>
2017-05-10 16:08:57 -0700

    Added compiled classes

--- src/Providers/OopsServiceProvider.php
+++ src/Providers/OopsServiceProvider.php
@@ -18,43 +18,50 @@ class OopsServiceProvider extends ServiceProvider {
    */
   protected $defer = false;
 
-  /**
-   * Register the service provider.
-   *
-   * @return void
-   */
-  public function register() {
-    app()->bind('talonon.ooops.dbcontext', DbContext::class);
-
-    app()->singleton(
-      'talonon.ooops.repositories', function () {
-      return new Collection();
-    });
-    app()->singleton(
-      'talonon.ooops.operations.overrides', function () {
-      return new Collection();
-    });
-
-
-    // Bind the generics
-    app()->bind('talonon.ooops.operations.rdbms.create', CreateOperation::class);
-    app()->bind('talonon.ooops.operations.rdbms.update', UpdateOperation::class);
-    app()->bind('talonon.ooops.operations.rdbms.delete', DeleteOperation::class);
-    app()->bind('talonon.ooops.operations.rdbms.get', GetSingleOperation::class);
-    app()->bind('talonon.ooops.operations.rdbms.getList', GetMultipleOperation::class);
-
-
-  }
-
-  public function boot() {
-    if (config('database.fetch') != \PDO::FETCH_ASSOC) {
-      config(['database.fetch' => \PDO::FETCH_ASSOC]);
-    }
-    $this->publishes(
-      [
-        __DIR__ . '/../../config/config.php' => config_path('operations.php'),
-      ]
-    );
+  public static function compiles() {
+    return [
+      __DIR__ . '/../Models/Entity.php',
+      __DIR__ . '/../Interfaces/ResultInterface.php',
+      __DIR__ . '/../Operations/BaseOperation.php',
+      __DIR__ . '/../Operations/RDBMS/BaseDbOperation.php',
+      __DIR__ . '/../Operations/RDBMS/BaseEntityOperation.php',
+      __DIR__ . '/../Operations/RDBMS/BaseGetOperation.php',
+      __DIR__ . '/../Operations/RDBMS/BaseSoftDeleteOperation.php',
+      __DIR__ . '/../Operations/RDBMS/CreateOperation.php',
+      __DIR__ . '/../Operations/RDBMS/UpdateOperation.php',
+      __DIR__ . '/../Operations/RDBMS/DeleteOperation.php',
+      __DIR__ . '/../Operations/RDBMS/GetSingleOperation.php',
+      __DIR__ . '/../Operations/RDBMS/GetMultipleOperation.php',
+      __DIR__ . '/../Models/BaseGetMultipleParams.php',
+      __DIR__ . '/../Models/BaseGetSingleParams.php',
+      __DIR__ . '/../Models/Struct.php',
+      __DIR__ . '/../Interfaces/HasTimestampsInterface.php',
+      __DIR__ . '/../Interfaces/HasUuidInterface.php',
+      __DIR__ . '/../Exceptions/InternalException.php',
+      __DIR__ . '/../Exceptions/ExternalException.php',
+      __DIR__ . '/../Exceptions/EntityNotFoundException.php',
+      __DIR__ . '/../Events/EventKeysStruct.php',
+      __DIR__ . '/../Traits/RDBMS/EntityCrud.php',
+      __DIR__ . '/../Database/Events/BeginTransaction.php',
+      __DIR__ . '/../Database/Events/CommitTransaction.php',
+      __DIR__ . '/../Database/Events/TransactionEvents.php',
+      __DIR__ . '/../Controllers/ResponseWithJson.php',
+      __DIR__ . '/../Controllers/ResponseWithView.php',
+      __DIR__ . '/../Controllers/BaseController.php',
+      __DIR__ . '/../Controllers/BaseViewController.php',
+      __DIR__ . '/../Controllers/BaseJsonController.php',
+      __DIR__ . '/../Repositories/BaseRepository.php',
+      __DIR__ . '/../Repositories/BaseDbRepository.php',
+      __DIR__ . '/../Repositories/BaseSoftDeleteDbRepository.php',
+      __DIR__ . '/../Traits/HasID.php',
+      __DIR__ . '/../Traits/HasName.php',
+      __DIR__ . '/../Traits/HasPagination.php',
+      __DIR__ . '/../Traits/HasTimestamps.php',
+      __DIR__ . '/../Traits/HasUuid.php',
+      __DIR__ . '/../Traits/ProvidesEvents.php',
+      __DIR__ . '/../Traits/ProvidesOverrides.php',
+      __DIR__ . '/../Traits/ProvidesRepositories.php',
+    ];
   }
 
   /**
@@ -73,6 +80,43 @@ class OopsServiceProvider extends ServiceProvider {
       'talonon.ooops.repositories',
       'talonon.ooops.dbcontext',
     ];
+  }
+
+  /**
+   * Register the service provider.
+   *
+   * @return void
+   */
+  public function register() {
+    app()->bind('talonon.ooops.dbcontext', DbContext::class);
+
+    app()->singleton(
+      'talonon.ooops.repositories', function () {
+      return new Collection();
+    });
+    app()->singleton(
+      'talonon.ooops.operations.overrides', function () {
+      return new Collection();
+    });
+
+    // Bind the generics
+    app()->bind('talonon.ooops.operations.rdbms.create', CreateOperation::class);
+    app()->bind('talonon.ooops.operations.rdbms.update', UpdateOperation::class);
+    app()->bind('talonon.ooops.operations.rdbms.delete', DeleteOperation::class);
+    app()->bind('talonon.ooops.operations.rdbms.get', GetSingleOperation::class);
+    app()->bind('talonon.ooops.operations.rdbms.getList', GetMultipleOperation::class);
+
+  }
+
+  public function boot() {
+    if (config('database.fetch') != \PDO::FETCH_ASSOC) {
+      config(['database.fetch' => \PDO::FETCH_ASSOC]);
+    }
+    $this->publishes(
+      [
+        __DIR__ . '/../../config/config.php' => config_path('operations.php'),
+      ]
+    );
   }
 
 }
