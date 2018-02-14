90eaa1df7e09494ee43e013eff88910004d6abe1
Andy Muth <andy@griffonco.com>
2018-02-13 21:49:05 -0800

    Do not log internal exceptions by default

--- src/Exceptions/InternalException.php
+++ src/Exceptions/InternalException.php
@@ -4,8 +4,10 @@ class InternalException extends \Exception {
   public function __construct($message = "", $code = 0, \Exception $previous = null) {
     $previousMessage = $previous instanceof \Exception ? $previous->getMessage() : '';
     $ip = isset($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : (isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : 'command line');
-    \Log::error($message, array('ip' => $ip, 'code' => $code, 'previous' => $previousMessage));
+    $this->logIt && \Log::error($message, array('ip' => $ip, 'code' => $code, 'previous' => $previousMessage));
     parent::__construct($message, $code, $previous);
   }
+
+  protected $logIt = false;
 }
 
